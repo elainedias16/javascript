02ff1f6d8c1f20ec177095916c445f0a1e84ce37
elainedias16 <elainediaspires16@gmail.com>
2022-07-15 23:38:56 -0300

    estudando

--- array.js
+++ array.js
@@ -11,3 +11,14 @@ console.log([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 //map
 //set
 //date
+
+
+const animals = [
+    'Lion',
+    'Monkey',
+    'Cat'
+]
+
+console.log(animals)
+
+console.log(animals[2])
