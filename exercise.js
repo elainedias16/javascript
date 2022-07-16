cd6c7d02983e49fe4dcabb1b6b96e20edae4d6e4
elainedias16 <elainediaspires16@gmail.com>
2022-07-15 23:41:46 -0300

    dever

---
+++ exercise.js
@@ -0,0 +1,39 @@
+let weight = 43
+
+console.log(typeof weight)
+
+
+const student  = {
+    name : 'joao' ,
+    age: 15,
+    stars : 3.4,
+    isSubscribed: false
+
+}
+
+
+console.log(`The student ${student.name} is ${student.age} years old`)
+
+let students =  []
+
+students = [
+    student
+]
+
+console.log(students)
+
+console.log(students[0])
+
+const joana = {
+    name: 'Joana' ,
+    age : 22,
+    stars : 4.4,
+    isSubscribed: false
+}
+
+students[1] = joana
+
+console.log(students)
+
+console.log(a)
+var a = 1
